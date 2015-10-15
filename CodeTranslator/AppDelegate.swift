2078080540c3b4ae129f792e464baa4a38aef6a1
Christopher Spencer <cmspencer109@gmail.com>
2015-10-15 17:30:24 -0500

    Initial Commit

---
+++ CodeTranslator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CodeTranslator
+//
+//  Created by Christopher Spencer on 10/15/15.
+//  Copyright (c) 2015 Christopher Spencer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CodeTranslator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CodeTranslator
+//
+//  Created by Christopher Spencer on 10/15/15.
+//  Copyright (c) 2015 Christopher Spencer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
